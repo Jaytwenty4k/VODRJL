56d93042337b6975ef11711b3dd3b82cd9239f94
Jayden1337 <JaydenWoppererYK@gmail.com>
2025-03-11 17:28:59 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+const CLIENT_ID = "1345009931509563443"; 
+const REDIRECT_URI = "https://jaytwenty4k.github.io/DanoneCraft/";
+
+document.addEventListener("DOMContentLoaded", () => {
+    checkForToken();
+    setupEventListeners();
+});
+
+function loginWithDiscord() {
+    const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=token&scope=identify`;
+    window.location.href = authUrl;
+}
+
+function getUserData(accessToken) {
+    fetch("https://discord.com/api/users/@me", {
+        headers: { "Authorization": `Bearer ${accessToken}` }
+    })
+    .then(response => response.json())
+    .then(user => {
+        const usernameElement = document.getElementById("username");
+        const avatarElement = document.getElementById("avatar");
+
+        if (usernameElement && avatarElement) {
+            usernameElement.textContent = user.username;
+            avatarElement.src = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+        }
+    })
+    .catch(error => console.error("Fehler beim Abrufen der Nutzerdaten:", error));
+}
+
+function checkForToken() {
+    const hash = window.location.hash;
+    if (hash.includes("access_token")) {
+        const params = new URLSearchParams(hash.substring(1));
+        const accessToken = params.get("access_token");
+        getUserData(accessToken);
+    }
+}
+
+function toggleMenu() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar) {
+        sidebar.style.right = sidebar.style.right === "0px" ? "-250px" : "0px";
+    }
+}
+
+function setupEventListeners() {
+    const menuButton = document.querySelector(".menu-btn");
+    const closeButton = document.querySelector(".close-btn");
+
+    if (menuButton) menuButton.addEventListener("click", toggleMenu);
+    if (closeButton) closeButton.addEventListener("click", toggleMenu);
+}
