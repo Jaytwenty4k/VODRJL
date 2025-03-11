2aca82f0392bf1e1d307e9c6c389413788afd0cc
Jayden1337 <JaydenWoppererYK@gmail.com>
2025-03-11 18:00:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,11 @@ function checkForToken() {
 
 function toggleMenu() {
     const sidebar = document.getElementById("sidebar");
-    sidebar.style.right = sidebar.style.right === "0px" ? "-250px" : "0px";
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-250px";
+    } else {
+        sidebar.style.left = "0px";
+    }
 }
 
 function setupEventListeners() {
